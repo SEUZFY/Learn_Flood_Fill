09ce942780449cfa3e9b9db6cd1871faec289c1b
zmocheng <zmocheng@gmail.com>
2022-04-20 00:29:56 +0200

    Add some code.

--- src/Learn_Flood_Fill.hpp
+++ src/Learn_Flood_Fill.hpp
@@ -21,5 +21,39 @@ bool inbbx(const CoordinateIndex& size, const CoordinateIndex& pos) {
 	return true;
 }
 
+/*
+* find neighours of one node - its location is: pos
+* 4 - connectivity: North, Sourth, West, East
+* Need to check whether the neighour's value is equal to original value
+* cuz in the traverse step the values of neighboring nodes may change
+* 
+* shape of grid:
+*   0 1 2 3 4 5 6
+* 0 * * * * * * *
+* 1 * * * * * * *
+* 2 * * * * * * *
+* 3 * * * * * * *
+* 4 * * * * * * *
+* 5 * * * * * * *
+* 6 * * * * * * *
+*/
+auto find_neighbours(
+	const std::vector<std::vector<double>>& grid,
+	CoordinateIndex pos,
+	double original_value,
+	double /* new_value */) {
+
+	// get four neighbours - CCW order
+	const std::vector<CoordinateIndex> possible_neighbours{
+		{pos.x, pos.y - 1}, // North
+		{pos.x + 1, pos.y}, // East
+		{pos.x, pos.y + 1}, // Sourth
+		{pos.x - 1, pos.y}  // West
+	};
+
+
+
+
+}
 
 
