82a546cd77e23a86bf4b358a63594b795ebc2852
zmocheng <zmocheng@gmail.com>
2022-04-20 11:54:15 +0200

    Change {} to ().

--- src/main.cpp
+++ src/main.cpp
@@ -25,11 +25,11 @@ int main() {
     std::vector<std::vector<double>> test_grid_left = grid;
     std::vector<std::vector<double>> test_grid_right = grid;
 
-    const CoordinateIndex start_left_pos{ 0, 2 }; // need to be set to the left side of the boundary
+    const CoordinateIndex start_left_pos(0, 2); // need to be set to the left side of the boundary
     DFS_fill(test_grid_left, start_left_pos, 0.0, 1.0); // after DFS fill, test_grid_left is changed
     assert(test_grid_left == solution_left_grid);
 
-    const CoordinateIndex start_right_pos{ 1, 3 }; // need to be set to the right side of the boundary
+    const CoordinateIndex start_right_pos(1, 3); // need to be set to the right side of the boundary
     DFS_fill(test_grid_right, start_right_pos, 0.0, 1.0); // after DFS fill, test_grid_right is changed
     assert(test_grid_right == solution_right_grid);
 
