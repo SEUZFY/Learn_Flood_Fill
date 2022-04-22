2d10dfc45c57e08e1d67e8ef2f2ec6d4a0b2c972
zmocheng <zmocheng@gmail.com>
2022-04-22 16:55:49 +0200

    Format code.

--- src/demo_lvalue_rvalue.hpp
+++ src/demo_lvalue_rvalue.hpp
@@ -9,9 +9,10 @@ public:
     {
         m_data = new int[size];
         m_size = size;
-        std::cout << "normal constructor called" << '\n';
+        std::cout << "constructor called" << '\n';
     }
 
+    // Rule of three --------------------------------------------------------------------------
     Holder(const Holder& other) { // copy constructor
         m_data = new int[other.m_size];
         std::copy(other.m_data, other.m_data + other.m_size, m_data);
@@ -19,6 +20,17 @@ public:
         std::cout << "copy constructor called" << '\n';
     }
 
+    /*
+    * const specifier: to avoid errorneous syntax
+    * e.g.
+    * \code
+    * 
+    * Holder h1(100);
+    * Holder h2(h1);
+    * (h1 = h2).print(); // avoid this confusing syntex
+    * 
+    * \endcode
+    */
     const Holder& operator=(const Holder& other) { // assignment operator
         if (this == &other)return *this;
         delete[] m_data;
@@ -35,6 +47,35 @@ public:
     {
         delete[] m_data;
     }
+    // Rule of three --------------------------------------------------------------------------
+
+
+    // Rule of five ----------------------------------------------------------------------------
+    Holder(Holder&& other)     // <-- rvalue reference in input
+    {
+        m_data = other.m_data;   // (1)
+        m_size = other.m_size;
+        other.m_data = nullptr;  // (2)
+        other.m_size = 0;
+        std::cout << "move constructor called" << '\n';
+    }
+
+    Holder& operator=(Holder&& other)     // <-- rvalue reference in input  
+    {
+        if (this == &other) return *this;
+
+        delete[] m_data;         
+
+        m_data = other.m_data;  // move resources
+        m_size = other.m_size;
+
+        other.m_data = nullptr;
+        other.m_size = 0;
+
+        std::cout << "move assignment called" << '\n';
+        return *this;
+    }
+    // Rule of five ----------------------------------------------------------------------------
 
     void print() {
         std::cout << "memory address: " << m_data << '\n';
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 
-#include "vector.hpp"
-#include "demo_lvalue_rvalue.hpp"
+//#include "vector.hpp"
+//#include "demo_lvalue_rvalue.hpp"
+#include "Learn_Flood_Fill.hpp"
 
 void process_2d_vector(float* dptr) {
 
@@ -38,6 +39,37 @@ int main() {
     //std::cout << "multiply vector: ";
     //multiply_vec.print();
 
+    const std::vector<std::vector<double>> grid{
+        {0, 0, 0, 1, 0},
+        {0, 0, 1, 0, 0},
+        {0, 0, 1, 0, 0},
+        {0, 0, 0, 1, 0},
+        {0, 0, 0, 1, 0} };
+
+    const std::vector<std::vector<double>> solution_left_grid{
+        {1, 1, 1, 1, 0},
+        {1, 1, 1, 0, 0},
+        {1, 1, 1, 0, 0},
+        {1, 1, 1, 1, 0},
+        {1, 1, 1, 1, 0} };
+
+    const std::vector<std::vector<double>> solution_right_grid{
+        {0, 0, 0, 1, 1},
+        {0, 0, 1, 1, 1},
+        {0, 0, 1, 1, 1},
+        {0, 0, 0, 1, 1},
+        {0, 0, 0, 1, 1} };
+
+    std::vector<std::vector<double>> test_grid_left = grid;
+    std::vector<std::vector<double>> test_grid_right = grid;
+
+    const CoordinateIndex start_left_pos(0, 2); // need to be set to the left side of the boundary
+    DFS_fill(test_grid_left, start_left_pos, 0.0, 1.0); // after DFS fill, test_grid_left is changed
+    assert(test_grid_left == solution_left_grid);
+
+    const CoordinateIndex start_right_pos(1, 3); // need to be set to the right side of the boundary
+    DFS_fill(test_grid_right, start_right_pos, 0.0, 1.0); // after DFS fill, test_grid_right is changed
+    assert(test_grid_right == solution_right_grid);
 
     return EXIT_SUCCESS;
 }
